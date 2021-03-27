0160daf123a2a50d236b0152628aee1527c05877
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-26 22:59:51 -0600

    still working on it

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,18 @@
 const mysql = require('mysql');
-
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
-  });
-}
-let connection = mysql.createConnection({
+connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'Stephen1000!',
   database: 'burgers_db',
 });
+}
+
 
 connection.connect((err) => {
   if (err) {
